129feda2e43112e52766ed5e779f232f2354cf47
tv-heck <tv-heck>
2026-08-25 09:57:10 -0300

    Adição da atividade de revisão

---
+++ 3º Bimestre/dados_strings/revisao_av1_formatador.py
@@ -0,0 +1,30 @@
+# Atividade 1 - Formatação de citação
+
+def formatar_citacao(nome_completo):
+     partes = nome_completo.strip().split()
+     sobrenome = partes[-1].upper()
+     primeiro_nome = " ".join(partes[:-1])
+     return sobrenome + ", " + primeiro_nome
+
+# Atividade 2 - Gerador de código de aluno
+
+def gerar_codigo(ano, cpf):
+     cpf_limpo = cpf.strip()
+     tres_digitos = cpf_limpo[0:3]
+     return "ALU-" + str(ano) + "-" + tres_digitos
+
+
+# Testes de fluxo
+
+# Teste 1 - Citação Bibliográfica
+
+autor = "Luca Bendô dos Santos"
+
+citacao_formatada = formatar_citacao(autor)
+print("Citação bibliográfica:", citacao_formatada)  # Saída esperada: "DOS SANTOS, Luca Bendô"
+
+# Teste 2 - Geração de código de aluno
+ano_matricula = 2024
+cpf_aluno = "123.456.789-00"
+codigo_gerado = gerar_codigo(ano_matricula, cpf_aluno)
+print("Matrícula gerada:", codigo_gerado)  # Saída esperada: "ALU-2024-123"
